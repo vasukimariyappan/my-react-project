482e09bd56bfd2d1c3e598b2b50ea5dc660431e0
Vasuki <vasuki.mariyappan28@gmail.com>
2026-09-08 17:54:17 +0530

    Updated React project and removed data.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import BasicIdentity from './Components/BasicIdentity'
 import Data from './Components/Data'
+import CustomerDataForm from './Components/CustomerDataForm'
 
 
 function App() {
   return(
     <>
-  <BasicIdentity />
+  <CustomerDataForm />
   <Data />
   </>
   );
